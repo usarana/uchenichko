366785230481b615da4dc288b39b90e7dd977cc2
usarana <policaicho@gmail.com>
2023-12-27 13:40:58 +0200

    Initial commit

---
+++ static/js/contentResolver.js
@@ -0,0 +1,14 @@
+// contentResolver.js
+
+// Зарежда страницата с избрания урок и предава id-то му като аргумент.
+function resolve(contentId) {
+    let loc = "content/";
+    // Страницата по съответния клиент
+    if(contentId.startsWith('m')) loc += 'math.html';
+    else if(contentId.startsWith('l')) loc += 'literature.html';
+    else if(contentId.startsWith('b')) loc += 'bulgarian.html';
+    // Добавяне на аргумент, премахване на буквата на дадения предмет.
+    loc += "?id=" + contentId.slice(1);
+    // Отваряне
+    window.location.href = loc;
+}
---
+++ static/js/displayContent.js
@@ -0,0 +1,36 @@
+// displayContent.js
+
+// Изобразяване на урока
+function display(subject) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const id = urlParams.get('id');
+    const divId = subject + id;
+    // Избиране на div-а, в който се съдържат материалите, които ни трябват
+    const div = document.getElementById(subject + id);
+    // Намиране на общ брой на уроци
+    let limit = 6;
+    if(subject == 'l') limit = 4;
+    else if(subject == 'm') limit = 8;
+    if(parseInt(id)>limit) document.getElementById('idErr').style.visibility = "visible";
+    // Изтриване на всички други уроци
+    for(let i = 1; i <= limit; i++) {
+        if(i == parseInt(id)) continue;
+        const div = document.getElementById(subject + i.toString());
+        //console.log(subject + i.toString());
+        div.remove();
+    }
+    // Показване на избрания урок. 
+    div.style.visibility = "visible";
+    renderMathInElement( // Изобразяване на LaTeX частите от урока, ако има такива
+        document.body,
+        {
+            delimiters: [
+                {left: "$$", right: "$$", display: true},
+                {left: "\\[", right: "\\]", display: true},
+                {left: "$", right: "$", display: false},
+                {left: "\\(", right: "\\)", display: false}
+            ]
+        }
+    );
+}
